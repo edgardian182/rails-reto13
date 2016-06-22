8990d10178f0780d77f6da27a58497af7632c6e0
Edgar Quintana <edgarquintana984@hotmail.com>
2016-06-22 15:45:16 -0500

    Create a has_and_belongs_to_many association (Product-Category)

--- db/migrate/20160622195712_create_products.rb
+++ db/migrate/20160622195712_create_products.rb
@@ -13,7 +13,7 @@ class CreateProducts < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    create_table :products_categories do |t|
+    create_table :categories_products do |t|
       t.belongs_to :product, index: true
       t.belongs_to :category, index:true
     end
