3f5b91805c279a74ae231749b162bd615702d367
Edgar Quintana <edgarquintana984@hotmail.com>
2016-06-22 16:23:30 -0500

    Define el controlador para Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,21 @@
 class ProductsController < ApplicationController
-  before_action :set_product, except: [:index, :create]
+  before_action :set_product, except: [:index, :create, :new]
 
   def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
   end
 
   def create
-
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path, notice: "The product was successfully created"
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -17,11 +27,16 @@ class ProductsController < ApplicationController
   end
 
   def update
-
+    if @product.update(product_params)
+      redirect_to root_path, notice: "The product was successfully updated"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @product.destroy
+    redirect_to root_path, notice: "The product was successfully deleted"
   end
 
   private
